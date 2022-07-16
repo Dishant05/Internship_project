1d94d48ef00afb5b4b8a137f4ce5cb16a90f80ee
Dishant05 <86092054+Dishant05@users.noreply.github.com>
2022-07-16 11:57:14 +0530

    Delete bot source code/bot source code directory

--- bot source code/bot source code/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qna-final-bot-8861-Web-Deploy.pubxml -p:Password=z5Z83rYkzePrNPt4d1WK95m2tsr0w3rloiT4yfEMbBdnDPxoopCDpvBt0fSr
-
